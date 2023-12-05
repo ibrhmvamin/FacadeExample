b5c3b73c6d14b63d742044222203d8fc47d95cd8
ibrhmvamin <aminibrahimov52@gmail.com>
2023-12-05 12:44:23 +0400

    Add project files.

---
+++ FacadeExample/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace FacadeExample
+{
+    
+    public class AudioPlayer
+    {
+        public void PlayAudio(string audioFile)
+        {
+            Console.WriteLine($"Playing audio: {audioFile}");
+        }
+    }
+
+   public class VideoPlayer
+    {
+        public void PlayVideo(string videoFile)
+        {
+            Console.WriteLine($"Playing video: {videoFile}");
+        }
+    }
+
+    public class Display
+    {
+        public void ShowOnScreen()
+        {
+            Console.WriteLine("Showing content on the screen");
+        }
+    }
+
+   public class MultimediaFacade
+    {
+        private AudioPlayer audioPlayer;
+        private VideoPlayer videoPlayer;
+        private Display display;
+
+        public MultimediaFacade()
+        {
+            audioPlayer = new AudioPlayer();
+            videoPlayer = new VideoPlayer();
+            display = new Display();
+        }
+
+        public void PlayMultimedia(string audioFile, string videoFile)
+        {
+            Console.WriteLine("Playing Multimedia:");
+            audioPlayer.PlayAudio(audioFile);
+            videoPlayer.PlayVideo(videoFile);
+            display.ShowOnScreen();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultimediaFacade multimediaFacade = new MultimediaFacade();
+            multimediaFacade.PlayMultimedia("song.mp3", "movie.mp4");
+        }
+    }
+
+}
